cfd80aefa7a6ef553a6d3bd8b48ab30213cf98d2
Alexandr <48907709+AlexandrMel@users.noreply.github.com>
2020-10-10 11:07:48 +0200

    Create project with Spring initializr
    Create project with Spring initializr

---
+++ src/test/java/com/example/Bank_Account_Toy/BankAccountToyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Bank_Account_Toy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankAccountToyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
